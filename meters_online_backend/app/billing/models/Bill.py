f36b46e1e5529976f953d219cbf8b163424bf208
pytbyte <heretolearn1@gmail.com>
2024-04-01 23:40:30 +0300

    Billing and invoicing: new bill genration

--- meters_online_backend/app/billing/models/Bill.py
+++ meters_online_backend/app/billing/models/Bill.py
@@ -1,9 +1,9 @@
 from app import db
-from ..WaterConstants import WaterConstants
+from sqlalchemy import Sequence
 
 class Bill(db.Model):
     __tablename__ = 'bills'
-    id = db.Column(db.Integer, primary_key=True)
+    bill_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     meter_id = db.Column(db.Integer, db.ForeignKey('meters.meter_id'))
     contact = db.Column(db.Integer, nullable=True)
     paid_ammount= db.Column(db.Integer, nullable=True)
@@ -17,60 +17,39 @@ class Bill(db.Model):
         db.DateTime,
         nullable=True,
         default=db.func.current_timestamp())
-    
+   
     
     meter = db.relationship('Meter', backref='Bill')
 
-    
-
-    def __init__(self, meter_id, units, status, amount, company_id, contact, ballance, paid_ammount):
-        self.meter_id = meter_id
-        self.units = units
-        self.status = status
-        self.amount = amount
-        self.company_id = company_id
-        self.contact = contact
-        self.ballance = ballance
-        self.paid_ammount = paid_ammount
-
-
-    
     def __repr__(self):
-        return f'<Bill {self.id}>'
+        return f'<Bill {self.bill_id}>'
     
-
     def delete(self):
         db.session.delete(self)
         db.session.commit()
         return True
     
-
     def save(self):
         db.session.add(self)
         db.session.commit()
         return self
     
-
-    def update(self):
+    def update(self):        
         db.session.commit()
         return True
         
-
     @staticmethod
     def get_all_bills():
         return Bill.query.all()
     
-
     @staticmethod
-    def get_bill_by_id(id):
-        return Bill.query.get(id)
+    def get_bill_by_id(bill_id):
+        return Bill.query.get(bill_id)
     
-
     @staticmethod
     def get_bill_by_customer_id(customer_id):
         return Bill.query.filter_by(customer_id=customer_id).all()
     
-
     @staticmethod
     def get_bill_by_company_id(company_id):
         return Bill.query.filter_by(company_id=company_id).all()
--- meters_online_backend/app/billing/views.py
+++ meters_online_backend/app/billing/views.py
@@ -5,12 +5,11 @@ from .models.MaBills import bill_schema, bills_schema
 from flask import request, jsonify,session
 from . .customer.models.Customer import Customer
 from .. meter.models.Meter import Meter
-from .. meter.models.MeterReading import MeterReading
-from .. meter.models.MaMeterReading import meter_reading_schema, meter_readings_schema
 from .. admin.models.Admin import Admins
 from .. customer.models.MaCustomer import customer_schema, customers_schema
 from marshmallow import ValidationError
 from flask_jwt_extended import jwt_required, get_jwt_identity
+import datetime
 
 
 
@@ -56,3 +55,69 @@ def delete_all_bills():
     for bill in bills:
         bill.delete()
     return jsonify(message='All bills deleted'), 200
+
+@bill.route('/<int:customer_id>', methods=['GET'])
+def get_bill_by_customer_id(customer_id):
+    bill = Bill.get_bill_by_id(customer_id)
+    if not bill:
+        return jsonify(message='Bill not found'), 404
+    bill_info = bills_schema.dump(bill)
+    return jsonify(bill=bill_info), 200
+
+
+
+@bill.route('/pay/', methods=['PUT'])
+@jwt_required()
+def update_bill():
+    """Update bill"""
+    try:
+        current_user = get_jwt_identity()
+        admin_id = current_user
+        admin_data = Admins.get_admin_by_id(admin_id)
+        company_id = admin_data.company_id
+        if not company_id:
+            return jsonify(message='Company ID not found in user identity'), 400
+
+        data = request.json
+        bill_id = data['bill_id']
+        if not bill_id:
+            return jsonify(message='bill ID is required'), 400
+
+        bill_ = Bill.query.filter_by(bill_id=bill_id, company_id=company_id).first()
+        if not bill_:
+            return jsonify(message='bill_ not found'), 404
+
+        before_payment = bill_.ballance
+        incoming_ammount = data['paid_ammount']
+        new_ballance = int(before_payment) - int(incoming_ammount)
+        
+         
+        
+        if new_ballance == 0:
+            status_ = "Cleared."
+            bill_.status = status_
+        else:
+            status_ = "uncleared"
+            bill_.status = status_
+
+        print("Bill id:", bill_.bill_id)
+        print("Before Payment:", before_payment)
+        print("Incoming Amount:", incoming_ammount)
+        print("New Balance:", new_ballance)
+
+
+
+       
+        bill_.paid_on = datetime.datetime.now()        
+        bill_.paid_ammount = incoming_ammount
+        bill_.ballance = new_ballance
+        bill_.update()
+
+        print("Bill updated successfully")
+
+        result = Bill.query.filter_by(company_id=company_id).order_by(Bill.bill_id.desc()).all()
+        return jsonify(bills_schema.dump(result)), 201
+
+    except Exception as e:
+        print("Error:", e)
+        return jsonify(message='An error occurred', error=str(e)), 500
